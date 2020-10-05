4688954f1a4bc140a721df9266aa15493d0fa3d1
elyashiv <elyashiv12@gmail.com>
2020-10-05 12:18:57 +0300

    Added setup.py

---
+++ tests/lib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smallpox',
+    version='1.0.0',
+    url='https://github.com/Elyash/smallpox.git',
+    author='Elyash',
+    author_email='elyashiv@gmail.com',
+    description='Description of my package',
+    packages=find_packages()
+)
